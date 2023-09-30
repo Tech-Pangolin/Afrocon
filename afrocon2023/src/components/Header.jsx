72bd9574d70f9fb2e94d9aaa9edddc6c3a434d2c
Grey Elerson <grey.l.elerson@gmail.com>
2023-09-29 19:08:35 -0600

    Create components

---
+++ afrocon2023/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header id="header" className="d-flex align-items-center ">
+            <div className="container-fluid d-flex align-items-center">
+                <div id="logo" className="me-auto">
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
